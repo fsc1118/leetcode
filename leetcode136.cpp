9e54861b6a3862505392fdac662788606a30466e
FangShicheng <fang282@purdue.edu>
2021-02-25 04:18:28 +0800

    leetcode136

---
+++ leetcode136.cpp
@@ -0,0 +1,12 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int i : nums){
+            result = result ^ i;
+        }
+        return result;
+    }
+};
